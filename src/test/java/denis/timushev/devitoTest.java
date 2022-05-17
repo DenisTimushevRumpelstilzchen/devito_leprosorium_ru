a89a2a842a7a6a80b68e9c5f82e1bd8eaa601137
Your NameDenis <tdv14011984@gmail.com>
2022-05-17 20:51:44 +0300

    upgrade test

--- src/test/java/denis/timushev/devitoTest.java
+++ src/test/java/denis/timushev/devitoTest.java
@@ -1,6 +1,7 @@
 package denis.timushev;
 
 import com.codeborne.selenide.Configuration;
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +9,11 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class devitoTest {
+
+    Faker faker = new Faker();
+
+    String Hello = faker.backToTheFuture().quote();
+    String email = faker.internet().emailAddress();
 
     @BeforeAll
     static void setUp() {
@@ -18,8 +24,8 @@ public class devitoTest {
     void searchLeproTest() {
         open("https://leprosorium.ru/login/");
         $(".b-login_navigation_devito").click();
-        $("[id=js-devito_post_body]").setValue("Hello!");
-        $("[id=js-devito_email]").setValue("sfhfghfghfgh@denni.com");
+        $("[id=js-devito_post_body]").setValue(Hello);
+        $("[id=js-devito_email]").setValue(email);
         $("[id=js-devito_form_submit]").click();
     }
 }
